88f35f3d6d434652eb6c912b473675ce18edfa4a
Simon <sweetsixteen.sudo@gmail.com>
2019-06-28 13:51:28 +0300

    Added design of login page

--- src/main/java/instagram/config/MvcСonfig.java
+++ src/main/java/instagram/config/MvcСonfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class MvcСonfig implements WebMvcConfigurer {
 
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/login").setViewName("login");
+        registry.addViewController("/login").setViewName("authpages/login");
     }
 
 }
--- src/main/java/instagram/controller/RegistrationController.java
+++ src/main/java/instagram/controller/RegistrationController.java
@@ -1,18 +1,12 @@
 package instagram.controller;
 
-import instagram.repository.UserRepo;
-import instagram.entity.Role;
 import instagram.entity.User;
 import instagram.service.RepositoryWorker;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.init.RepositoriesPopulatedEvent;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-
-import java.util.Collections;
 
 @Controller
 @RequiredArgsConstructor
@@ -21,7 +15,7 @@ public class RegistrationController {
 
     @GetMapping(value = "/registration")
     public String registration() {
-        return "registration";
+        return "authpages/registration";
     }
 
     @PostMapping(value = "/registration")
@@ -29,7 +23,7 @@ public class RegistrationController {
         boolean exist = repositoryWorker.checkIfUserExist(user.getUsername());
         if (exist) {
             model.addAttribute("message", "User exists");
-            return "registration";
+            return "authpages/registration";
         }
         repositoryWorker.saveUser(user);
         return "redirect:/login";
